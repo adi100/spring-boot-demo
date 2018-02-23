72c25a6d4a2c2976f061fe3a69c7292e1709a5d1
liubo <liubo@urwork.cn>
2018-02-23 09:52:52 +0800

    document指定collection

--- src/main/java/com/example/demo/model/mongo/MongoUser.java
+++ src/main/java/com/example/demo/model/mongo/MongoUser.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 @Setter@Getter
-@Document
+@Document(collection = "user")
 public class MongoUser {
     @Field("id")
     private Integer id;
